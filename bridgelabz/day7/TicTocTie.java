e7812187a6450db12f8268be2e4d8a3b6514524c
Bharti <bharatifule10gmail.com>
2023-01-20 17:02:53 +0530

    Added:Tic toc tie problems

--- bridgelabz/day7/TicTocTie.java
+++ bridgelabz/day7/TicTocTie.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 
 public class TicTocTie {
 	public static void main(String[] args) {
-        System.out.println("Welcome to tic toc tie problems ");
+		System.out.println("Welcome to tic toc tie problems");
        createEmptyBoard();
        allowPlayer();
+       showBoard();
     }
     public static void createEmptyBoard(){
-        char[] sizeOfBoard = new char[10];
-      
+
+        System.out.println(" ____________");
         for (int i = 1; i <10; i++){
             System.out.print("|___");
             if (i % 3 == 0) {
                 System.out.println("|");
             }
-         }    
+        }
       }
-       public static void allowPlayer(){
+    public static void allowPlayer(){
         System.out.println("Enter your choice if wont 'X' then press 1 & if you wont 'O' then press 2");
         Scanner sc = new Scanner(System.in);
         int choice = sc.nextInt();
@@ -28,6 +29,17 @@ public class TicTocTie {
         } else if (choice == 2) {
             System.out.println(" player => O");
             System.out.println("Computer => X");
-         }
         }
+
+    }
+    public static void showBoard(){
+        char[] arr = {48,49,50,51,52,53,54,55,56,57};
+     
+        for (int i = 1; i <=9; i++){
+            System.out.print("|_"+arr[i]+"_");
+            if (i % 3 == 0) {
+                System.out.println("|");
+            }
+        }
+    }
        }
